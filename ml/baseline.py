8fa0d47cb12e993747ca529e60fad3806878e33b
Asher Stout <stoutashe@duck-island.ecs.vuw.ac.nz>
2022-12-09 12:42:54 +1300

    Added SVC & KNN, adapted logs for non-probability support

--- ml/baseline.py
+++ ml/baseline.py
@@ -1,12 +1,15 @@
 import logging
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, roc_auc_score
+import numpy as np
 import numpy.random as nprand
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 
-def log_results(preds, trues, probs, name="?Model?"):
+def log_results(preds, trues, probs=[], name="?Model?"):
     """
     Logs classifier predictions and provides formatted statistics about performance
 
@@ -15,10 +18,9 @@ def log_results(preds, trues, probs, name="?Model?"):
     :param probs: predicted label probabilities
     :param name: string identifier of model
     """
-    logging.info("{name} obtained results".format(name=name))
     results = {}
     results.update({"Accuracy": accuracy_score(trues, preds)})
-    results.update({"ROC AUC": roc_auc_score(trues, probs, multi_class="ovo")})
+    results.update({"ROC AUC": np.NaN if len(probs) == 0 else roc_auc_score(trues, probs, multi_class="ovo")})
     return results
 
 
@@ -79,6 +81,43 @@ def dtree(x_data, x_labels, y_data, y_labels, **kwargs):
     return res
 
 
+def knn(x_data, x_labels, y_data, y_labels, **kwargs):
+    """
+    K-Nearest Neighbor algorithm
+
+    :param x_data: droplet evaporation sequence training data
+    :param x_labels: training labels
+    :param y_data: droplet evaporation sequence testing data
+    :param y_labels: testing labels
+    :return: statistics on model performance
+    """
+    kn = KNeighborsClassifier()
+    kn.fit(x_data, x_labels)
+
+    preds = kn.predict(y_data)
+    probs = kn.predict_proba(y_data)
+    res = log_results(preds, y_labels, probs, name="KNN")
+    return res
+
+
+def svc(x_data, x_labels, y_data, y_labels, **kwargs):
+    """
+    Support Vector Classifier
+
+    :param x_data: droplet evaporation sequence training data
+    :param x_labels: training labels
+    :param y_data: droplet evaporation sequence testing data
+    :param y_labels: testing labels
+    :return: statistics on model performance
+    """
+    vec = SVC()
+    vec.fit(x_data, x_labels)
+
+    preds = vec.predict(y_data)
+    res = log_results(preds, y_labels, name="SVC")
+    return res
+
+
 def _seed_decorator(func):
     """
     Decorator for setting up seed values outside of model functions
@@ -93,5 +132,6 @@ def _seed_decorator(func):
     return execute
 
 
-models = {"logreg": [_seed_decorator(logreg)], "nbayes": [_seed_decorator(nbayes)], "dt": [_seed_decorator(dtree)]}
-models.update({"all": [_seed_decorator(logreg), _seed_decorator(nbayes), _seed_decorator(dtree)]})
+models = {"logreg": [logreg], "nbayes": [nbayes], "dt": [dtree], "knn": [knn], "svc": [svc]}
+models.update({"all": [logreg, nbayes, dtree, knn, svc]})
+[models.update({k: [_seed_decorator(m) for m in models[k]]}) for k in models.keys()]
