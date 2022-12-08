3633b812ba01e1602639907826eee9b55e5e4a7a
Asher Stout <stoutashe@duck-island.ecs.vuw.ac.nz>
2022-12-08 14:19:05 +1300

    Added LR & NB methods, added preprocessed csvs

--- ml/main.py
+++ ml/main.py
@@ -1,4 +1,11 @@
+"""
+python main.py --type processed --model
+"""
+import os
 from argparse import ArgumentParser
+from methods import models
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 
 def define_arguments():
@@ -8,23 +15,49 @@ def define_arguments():
     :return: default arguments
     """
     a = ArgumentParser()
-    a.add_argument('--mode', default='logreg', type=str, help='Mode ')
+    a.add_argument('--model', default='logreg', type=str, help='ML classification model')
+    a.add_argument('--dir', default='../data/processed', type=str, help='Path to data folders')
+    a.add_argument('--type', default='processed', type=str, help='Observations contain a raw and processed csv file')
     a = a.parse_args()
     return a
 
 
-def load():
+def load(data_dir, data_type):
     """
-    Loads preprocessed samples of droplet sequences.
-
+    Loads preprocessed samples of droplet sequences and converts them into
     :return:
     """
-    data = []
-    return data
+    d = []
+    l = []
+    classes = os.listdir(data_dir)
+    for c in classes:
+        class_dir = data_dir + "/" + c
+        insts = []
+        for f in os.listdir(class_dir+"/"):  # individually load each .csv file
+            file_dir = class_dir+"/"+f+"/"
+            files = os.listdir(file_dir)
+            file = list(filter(lambda fl: data_type+".csv" in fl, files))
+            file = file[0]
+            insts.append(pd.read_csv(file_dir+file))
+        insts = [i.iloc[5:200:5, 2:] for i in insts]  # only every 5th value in first 200 timesteps, starting from 5
+
+        to_avg = ["dl_height_midpoint", "2l_to_2r", "1l_to_1r"]
+        for i in insts:  # get mean over centre 3 observations
+            i["midpoints_mean"] = i[to_avg].mean(axis=1)
+        insts = [i.drop(to_avg, axis=1) for i in insts]  # then drop original observations
+        insts = [i.to_numpy().flatten() for i in insts]  # flatten instances
+        d += insts
+        l += [c] * len(insts)
+    d = pd.DataFrame(d)  # then convert back into dataframe
+    d = d.fillna(0)  # 0-imputation
+    return d, l
 
 
-# Delegate model to correct mode, using the provided arguments.
+# Delegate mode to correct model, using the provided arguments.
 if __name__ == '__main__':
-    # Load droplet data
+    args = define_arguments()
 
-    pass
+    data, labels = load(args.dir, args.type)
+    train_d, test_d, train_l, test_l = train_test_split(data, labels, test_size=0.3)
+
+    models[args.model](train_d, train_l, test_d, test_l)
--- ml/methods.py
+++ ml/methods.py
@@ -2,21 +2,39 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 
 
-def logreg(data):
+def logreg(x_data, x_labels, y_data, y_labels, **kwargs):
     """
     Logistic Regression model
 
-    :param data: processed sequence data
-    :return:
+    :param x_data: droplet evaporation sequence training data
+    :param x_labels: training labels
+    :param y_data: droplet evaporation sequence testing data
+    :param y_labels: testing labels
     """
+    lr = LogisticRegression(random_state=444, max_iter=1000)
+    lr.fit(x_data, x_labels)
+
+    # preds = lr.predict(y_data)
+    acc = lr.score(y_data, y_labels)
+    print(acc)
     return None
 
 
-def bayes(data):
+def nbayes(x_data, x_labels, y_data, y_labels, **kwargs):
     """
     Naive Bayes model
 
-    :param data: processed sequence data
-    :return:
+    :param x_data: droplet evaporation sequence training data
+    :param x_labels: training labels
+    :param y_data: droplet evaporation sequence testing data
+    :param y_labels: testing labels
     """
+    nb = GaussianNB()
+    nb.fit(x_data, x_labels)
+
+    acc = nb.score(y_data, y_labels)
+    print(acc)
     return None
+
+
+models = {"logreg": logreg, "nbayes": nbayes}
