c9a2e6a5aa2505f2f468c7d3bdbb0e2de24d5f0c
Asher Stout <stoutashe@duck-island.ecs.vuw.ac.nz>
2022-12-15 11:48:53 +1300

    Reduced code repetition in visuals, more figures + better colour heatmaps

--- visuals/visuals.py
+++ visuals/visuals.py
@@ -7,7 +7,7 @@ import os
 import re
 from argparse import ArgumentParser, BooleanOptionalAction
 
-import numpy
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -41,10 +41,8 @@ def load_model_expr(path):
     :param path: path to experiment directory
     :return:
     """
-    files = os.listdir(path)
-    files = [file for file in files if ".csv" in file]
-    files.sort(key=lambda f: int(re.split("[_.]", f)[-2]))
-    lines = list(reversed([pd.read_csv(path+file).iloc[:, 1:3] for file in files]))
+    files = _load_and_sort(path, ".csv", sort=True)
+    lines = list(reversed([pd.read_csv(path+file).iloc[:, 1:] for file in files]))
     lines = pd.concat(lines, ignore_index=True)
 
     lines["Timestep"] = list(reversed(_time_steps()))  # these are hardcoded; remove ASAP
@@ -59,24 +57,13 @@ def load_model_expr_importances(path, processed=False):
     :param processed: when "processed" columns are duplicated to maintain symmetry when visualized
     :return:
     """
-
-    files = os.listdir(path)
-    files = [file for file in files if ".csv" in file]
+    files = _load_and_sort(path, ".csv")
     lines = list(reversed([pd.read_csv(path+file).iloc[:, 1:] for file in files]))
     lines = pd.concat(lines, ignore_index=True)
     if processed:
         lines = pd.concat([lines.iloc[:, :], lines.iloc[:, 14::-1]], axis=1)
         lines.columns = [str(i) for i in range(0, len(lines.columns))]
     return lines
-
-
-def _time_steps():
-    """
-    Hardcoded list of observed timestamps; TODO: should be inferred from valid files
-
-    :return:
-    """
-    return [0, 25, 50, 75, 100, 150, 200, 300, 400, 500, 600, 700, 800, 900]
 
 
 def _load_droplet_example_shadow():
@@ -90,6 +77,31 @@ def _load_droplet_example_shadow():
     shadow_data = shadow_data.iloc[:, 4:]
     shadow_data = shadow_data[reshaped_order]
     return shadow_data
+
+
+def _time_steps():
+    """
+    Hardcoded list of observed timestamps; TODO: should be inferred from valid files
+
+    :return:
+    """
+    return [0, 25, 50, 75, 100, 150, 200, 300, 400, 500, 600, 700, 800, 900]
+
+
+def _load_and_sort(path, ext, sort=False):
+    """
+    Loads and sorts alphanumerically files from the provided directory.
+
+    :param path: directory to load files
+    :param ext: allowed file extension
+    :param sort: True sorts file names alphanumerically
+    :return:
+    """
+    files = os.listdir(path)
+    files = [file for file in files if ext in file]
+    if sort:
+        files.sort(key=lambda f: int(re.split("[_.]", f)[-2]))
+    return files
 
 
 def accuracy_plot(mod, names, metric):
@@ -132,17 +144,22 @@ def importance_plot(t, data, ref_data, ref_norm, axis):
     plt.fill_between(list(range(0, cols)), data.iloc[t, :], ref_row.iloc[0, :], color='black', alpha=0.1)
 
 
-def covar_heatmap(t, mat):
+def covar_heatmap(t, mat, mod, r):
     """
     Produces a heatmap of calculated feature covariances at a provided timestep
 
     :param t: indexed timestep to observe covariance at
     :param mat: iterable of covariance matrices
+    :param mod: model name (for use in title)
+    :param r: bounds of pixel values in the heatmap
     :return:
     """
+    assert len(r) == 2
     plt.clf()
     t -= 1
-    sns.heatmap(mat[t])
+    cm = sns.cubehelix_palette(start=.5, rot=-.75, as_cmap=True, reverse=True)
+    sns.heatmap(mat[t], xticklabels=[], yticklabels=[], vmin=r[0], vmax=r[1], cmap=cm)
+    plt.title("{m} Cov at {T}".format(m=mod, T=str(_time_steps()[t])))
 
 
 def write_to_anim(figure, a, f, t, out):
@@ -173,7 +190,6 @@ if __name__ == '__main__':
         "xtick.color": tick_color,
         "ytick.color": tick_color,
         "axes.edgecolor": grid_color,
-
         "axes.labelcolor": text_color,
         "grid.color": grid_color,
     }
@@ -181,53 +197,54 @@ if __name__ == '__main__':
 
     # Setup import & export paths
     args = define_arguments()
-    indir = "../logs/{pref}/".format(pref=args.data)
-    outdir = "../output/figures/"
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    outdir = "{od}{pref}/".format(od=outdir, pref=args.data)  # ensure sub-directory also exists
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    folders = os.listdir(indir)
-    folders = sorted([f for f in folders if "txt" not in f])
     is_processed = True if args.data == "processed" else False
+
+    in_dir = "../logs/{pref}/".format(pref=args.data)
+    folders = os.listdir(in_dir)
+    folders = sorted([f for f in folders if "txt" not in f])
+
+    out_dir = "../output/figures/"
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    out_dir = "{od}{pref}/".format(od=out_dir, pref=args.data)  # ensure subdirectories also exists
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
 
     # Accuracy figure
     acc_models = []
     for model in folders:   # accuracies
-        acc_models.append(load_model_expr(indir + model + "/"))
+        acc_models.append(load_model_expr(in_dir + model + "/"))
     accuracy_plot(acc_models, folders, args.metric)
-    plt.savefig(f'{outdir}{args.data}_acc.png')
+    plt.savefig(f'{out_dir}{args.data}_{args.metric}.png')
     plt.clf()
 
     # Importance Figure
     for model in folders:
+        importance_path = in_dir + model + "/importance/"
         if model in ["dt", "logreg"] and args.importance:
             fig, ax = plt.subplots()
-            importance = load_model_expr_importances(indir + model + "/importance/", processed=is_processed)
+            importance = load_model_expr_importances(importance_path, processed=is_processed)
             shadow = _load_droplet_example_shadow()
             animate = partial(importance_plot, data=importance, ref_data=shadow, ref_norm=max(shadow.iloc[0, :])*3,
                               axis=ax)
 
             fps, time = 1, len(_time_steps())
-            gif_name = f'{outdir}{model}_{args.data}_fi.gif'
+            gif_name = f'{out_dir}{model}_{args.data}_fi.gif'
             write_to_anim(fig, animate, fps, time, gif_name)
 
     # Covariance Figure
     for model in folders:  # feature covariances
+        importance_path = in_dir + model + "/importance/"
         if model in ["dt", "logreg"] and args.covariance:
             fig, ax = plt.subplots()
-
-            covars = []
-            np_files = os.listdir(indir + model + "/importance/")
-            np_files = [file for file in np_files if ".npy" in file]
-            np_files.sort(key=lambda f: int(re.split("[_.]", f)[-2]))
+            np_files = _load_and_sort(importance_path, ".npy", sort=True)
+            cvs = []
             for numpy_file in np_files:
-                if ".npy" in numpy_file:
-                    cov = numpy.load(indir + model + "/importance/"+numpy_file)
-                    covars.append(cov)
-            animate = partial(covar_heatmap, mat=covars)
+                cov = np.load(importance_path + numpy_file)
+                cvs.append(cov)
+            vmin, vmax = min([np.min(c) for c in cvs]), max([np.max(c) for c in cvs])
+            animate = partial(covar_heatmap, mat=cvs, mod=model, r=(vmin, vmax))
 
             fps, time = 1, len(_time_steps())
-            gif_name = f'{outdir}{model}_{args.data}_cov.gif'
+            gif_name = f'{out_dir}{model}_{args.data}_cov.gif'
             write_to_anim(fig, animate, fps, time, gif_name)
